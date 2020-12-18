3207f97ccf396814a38b3f368b5575e2c15b8440
Leina McDermott <leina05@gmail.com>
2020-12-18 17:29:26 -0500

    add policy checks

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, g, request
+from flask import Blueprint, g, request, current_app
 from .models import User, Organization, Repository
 
 bp = Blueprint("routes", __name__)
@@ -12,6 +12,7 @@ def whoami():
 @bp.route("/orgs/<int:org_id>/repos", methods=["GET"])
 def repos_index(org_id):
     org = g.session.query(Organization).filter(Organization.id == org_id).first()
+    current_app.oso.authorize(org, actor=g.current_user, action="LIST_REPOS")
 
     repos = g.session.query(Repository).filter_by(organization=org)
     return {f"repos": [repo.repr() for repo in repos]}
@@ -20,4 +21,5 @@ def repos_index(org_id):
 @bp.route("/orgs/<int:org_id>/billing", methods=["GET"])
 def billing_show(org_id):
     org = g.session.query(Organization).filter(Organization.id == org_id).first()
+    current_app.oso.authorize(org, actor=g.current_user, action="READ_BILLING")
     return {f"billing_address": org.billing_address}
