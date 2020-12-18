fe6ecf4e33a7bb091a70efda9e31b6cadae26a86
Leina McDermott <leina05@gmail.com>
2020-12-18 12:12:00 -0500

    move before_app_request into __init__

--- app/__init__.py
+++ app/__init__.py
@@ -2,15 +2,15 @@ from flask import g, Flask, request
 
 from datetime import datetime, timedelta
 
-from .models import Base
+from .models import Base, User
 from .fixtures import load_fixture_data
 from .db import engine, Session
-from .authorization import init_oso
+
+from werkzeug.exceptions import Unauthorized
 
 
 def create_app():
     app = Flask(__name__)
-    init_oso(app)
 
     Base.metadata.create_all(engine)
 
@@ -20,5 +20,20 @@ def create_app():
     from . import routes
 
     app.register_blueprint(routes.bp)
+
+    @app.before_request
+    def set_session_and_user():
+        if "current_user" not in g:
+            email = request.headers.get("user")
+            if not email:
+                return Unauthorized("user not found")
+            try:
+                basic_session = Session()
+                g.basic_session = basic_session
+                g.current_user = (
+                    basic_session.query(User).filter(User.email == email).first()
+                )
+            except Exception as e:
+                return Unauthorized("user not found")
 
     return app
