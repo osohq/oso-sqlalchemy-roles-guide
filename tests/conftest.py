236c5946126bb55d8d248ccae60374f188dbf411
Leina McDermott <leina05@gmail.com>
2020-12-21 12:03:37 -0500

    remove cruft from rebase

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,7 @@ import pytest
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from app import create_app, models, engine, Session
+from app import create_app, models
 from app.fixtures import load_fixture_data
 
 
